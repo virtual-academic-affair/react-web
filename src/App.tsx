ab450ad42512a41529a2308e6894e2889aaaed21
Tai Trinh <tatai22@clc.fitus.edu.vn>
2026-02-06 06:31:42 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-3xl font-bold text-red-600 underline">
+        Hello Vite + React!
+      </h1>
+    </div>
+  );
+}
