c3410d0c00d8a345af48d8f6b136501a122adbad
Tai Trinh <tatai22@clc.fitus.edu.vn>
2026-02-22 16:39:59 +0700

    fix: sửa lỗi border radius, outline của card component; chỉnh font mặc định về Momo Trust Sans

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -7,7 +7,7 @@ function Card(props: {
   const { variant, extra, children, ...rest } = props;
   return (
     <div
-      className={`relative flex flex-col rounded-[10px] border-[1px] border-gray-200 bg-white bg-clip-border shadow-md shadow-[#F3F3F3] dark:border-[#ffffff33] dark:!bg-navy-800 dark:text-white dark:shadow-none ${extra}`}
+      className={`dark:!bg-navy-800 relative flex flex-col rounded-3xl border-gray-200 bg-white bg-clip-border shadow-md shadow-[#F3F3F3] dark:border-[#ffffff33] dark:text-white dark:shadow-none ${extra}`}
       {...rest}
     >
       {children}
